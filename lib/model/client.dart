4c55e55187a1abb239e922f46223e41a5abdc945
Dmytro Kovalenko <koditka@gmail.com>
2021-02-20 18:36:21 +0200

    added calculations; remained turn of number to secretKey of bytes

--- lib/model/client.dart
+++ lib/model/client.dart
@@ -1,15 +1,13 @@
-import 'dart:convert';
-
 import 'package:cryptography/cryptography.dart';
 import 'package:cypher_app/model/asymetric_message_cypher.dart';
 import 'package:cypher_app/model/package.dart';
+import 'dart:math';
 
 class Client {
   String name;
   List<Package> directory;
   Nonce openKey; //IV
   SecretKey _secretKey;
-  List<Client> connectedClients;
 
   Client._(this.name);
 
@@ -20,8 +18,27 @@ class Client {
     return client;
   }
 
+  Future<int> getMidNumber(int prime, int n) async {
+    return pow(prime, _bytesToInteger(await _secretKey.extract())) % n;
+  }
+
+  void calculateSecretKey(int prime, int n, int midNum) async {
+    final commonKey =
+        pow(midNum, _bytesToInteger(await _secretKey.extract())) % n;
+  }
+
   Future<void> sendMessage(Client recipient, String message) async {
     final encryptedMessage = AsymetricMessageCypher.encryptMessage();
     final List list = await _secretKey.extract();
+  }
+
+  int _bytesToInteger(List<int> bytes) {
+    var value = 0;
+
+    for (var i = 0, length = bytes.length; i < length; i++) {
+      value += bytes[i] * pow(256, i);
+    }
+
+    return value;
   }
 }
--- lib/view/states/app_state.dart
+++ lib/view/states/app_state.dart
@@ -3,9 +3,16 @@ import 'package:cypher_app/model/package.dart';
 import 'package:flutter/widgets.dart';
 
 class AppState with ChangeNotifier {
-  List<Client> clients;
+  Client alice = Client("Alice");
+  Client bob = Client("Bob");
 
-  void sendMessage(Client sender, Client recipient, String message) {
+  void sendMessage(Client sender, Client recipient, String message) async {
+    final int primeNumber = 7;
+    final int n = 11;
+
+    final midSender = await sender.getMidNumber(primeNumber, n);
+    final midRecipient = await recipient.getMidNumber(primeNumber, n);
+
     final package = Package(
       sender: sender,
       recipient: recipient,
